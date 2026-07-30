c61d6672b452c34e31f99f2f7a323ffdacce485d
Vansh <singhmoksh207@gmail.com>
2026-07-30 15:34:52 +0530

    REMOVED FETCH LOCAL

--- backend/index.js
+++ backend/index.js
@@ -62,7 +62,7 @@ async function generateTimetableFlow() {
     }
 
     try {
-        const response = await fetch('http://127.0.0.1:8000/api/timetable', {
+        const response = await fetch('/api/timetable', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ subjects: savedSubjects })
@@ -114,7 +114,7 @@ function filterTimetableByDay(selectedDay) {
 
 async function fetchSubjects(semesterId) {
     try {
-        const response = await fetch(`http://127.0.0.1:8000/api/subjects`, {
+        const response = await fetch(`/api/subjects`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ semester: semesterId })
@@ -123,7 +123,7 @@ async function fetchSubjects(semesterId) {
         const filteredSem = await response.json();
         const baseSubjectNames = filteredSem.map(sub => sub.subjectName);
 
-        const ttResponse = await fetch(`http://127.0.0.1:8000/api/timetable`, {
+        const ttResponse = await fetch(`/api/timetable`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ subjects: baseSubjectNames })
